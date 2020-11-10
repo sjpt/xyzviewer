7f3c650ccb99c4ef5de7084ad80d2cf031b17a11
WINDOWS-4PQGKTN\Organic <stephentodd@gmail.com>
2020-11-10 16:25:32 +0000

    more speech commands, and hoverhelp

--- xyzspeech.js
+++ xyzspeech.js
@@ -1,6 +1,6 @@
 // speech input for xyz
 //  
-var OrganicSpeech, controls, maingroup;
+var OrganicSpeech, controls, maingroup, orbcamera;
 //OrganicSpeech.commands = {};
 //OrganicSpeech.replace = {};
 
@@ -9,22 +9,46 @@ var OrganicSpeech, controls, maingroup;
     let rate = 1;
     let panrate = 0.1;
     let o = OrganicSpeech;
-    o.commands.forward = () => mode = 'forward';
-    o.commands.back = () => mode = 'back';
-    o.commands.stop = () => mode = '';
-    o.commands.faster = () => rate *= 2;
-    o.commands.slower = () => rate /= 2;
-    o.commands.elevation = () => maingroup.rotation.set(Math.PI/2,0,0)
-    o.commands.plan = () => maingroup.rotation.set(0,0,0)
+    let c = o.commands;
+    'forward back left right up down'.split(' ').forEach(k => c[k] = () => mode = k);
+    c.stop = () => mode = '';
+    c.faster = () => rate *= 2;
+    c.slower = () => rate /= 2;
+    c.elevation = () => maingroup.rotation.set(Math.PI/2,0,0)
+    c.plan = () => maingroup.rotation.set(0,0,0)
+    c['go to centre'] = () => orbcamera.position.set(0,0,0)
+    c['go to outside'] = () => orbcamera.position.set(0,0,30)
+    c['look at centre'] = () => controls.target.set(0,0,0)
 
     window.xyzspeechupdate = () => {
-        switch(mode) {
-            case 'forward': controls.panForward(-rate * panrate); break;
-            case 'back': controls.panForward(rate * panrate); break;
+        try {
+            switch(mode) {
+                case 'forward': controls.panForward(-rate * panrate); break;
+                case 'back': controls.panForward(rate * panrate); break;
+                case 'left': controls.panLeft(rate * panrate); break;
+                case 'right': controls.panLeft(-rate * panrate); break;
+                case 'up': controls.panUp(rate * panrate); break;
+                case 'down': controls.panUp(-rate * panrate); break;
+            }
+        } catch (e) {
+            console.error('bad command', mode, e);
         }
     }
+
+    window.speechhelp.innerHTML = `
+Check box for speech input.<br>
+Available commands:<br>
+<ul><li>
+${Object.keys(c).join('</li><li>')}
+</li></ul>
+`
 }
 
-
+/**
+ * note: 
+ * to change camera position,  orbcamera.position.set(x,y,z)
+ * to change look at,          
+ * 
+ */
 
 
